7d308eb6aaff99a4e5b75f066a835bf4bbc4409c
Vincent_Liussyaputra <liusvinv@gmail.com>
2023-09-12 04:17:37 +0700

    Remove optimization

--- next.config.js
+++ next.config.js
@@ -3,22 +3,23 @@
  *  */
 const nextConfig = {
 	images:{
-		remotePatterns:[
-			{
-				protocol:'https',
-				hostname:'gogocdn.net',
-			},{
-				protocol:'https',
-				hostname:'s4.anilist.co'
-			},{
-				protocol:'https',
-				hostname:'artworks.thetvdb.com'
-			},{
-				protocol:'https',
-				hostname:'media.kitsu.io'
-			}
-		]
-	}
+		// remotePatterns:[
+		// 	{
+		// 		protocol:'https',
+		// 		hostname:'gogocdn.net',
+		// 	},{
+		// 		protocol:'https',
+		// 		hostname:'s4.anilist.co'
+		// 	},{
+		// 		protocol:'https',
+		// 		hostname:'artworks.thetvdb.com'
+		// 	},{
+		// 		protocol:'https',
+		// 		hostname:'media.kitsu.io'
+		// 	}
+		// ],
+		unoptimized:true
+	},
 }
 
 module.exports = nextConfig
