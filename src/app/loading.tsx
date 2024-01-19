7872e58b8ed88b555d42e62453a6a6fe90b0bbc3
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-01-20 03:15:58 +0700

    Fix styling on anime list

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,15 +1,3 @@
-'use client';
-import React from 'react';
-import { FaSpinner } from 'react-icons/fa';
-// import './loading.scss';
-import { AnimatePresence } from 'framer-motion';
-import {motion} from 'framer-motion';;
+import RootLoading from "@/components/general/loading";
 
-type Props = {}
-export default function RootLoading({}: Props) {
-  return (
-		<motion.div className='loading-bar' initial={{opacity:0}} animate={{opacity:0.5}} transition={{duration:5}} exit={{opacity:0}} key={'loading-screen'}>
-				{/* <FaSpinner/> */}
-		</motion.div>
-  );
-}
+export default RootLoading;
